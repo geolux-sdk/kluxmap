797b980db1d9951cf6ac12ada2d724e85d2a0b5d
SD KIM <sdkim@geolux.co.kr>
2025-11-03 13:01:31 +0900

    one-file 삭제

--- nuitka_gen_ex.bat
+++ nuitka_gen_ex.bat
@@ -1,7 +1,7 @@
 nuitka KMagHunters.py ^
+--standalone ^
 --mingw64 --jobs=8 ^
 --plugin-enable=pyside6 ^
---onefile ^
 --file-reference-choice=runtime ^
 --deployment ^
 --include-data-files=./img/*.png=./img/ ^
